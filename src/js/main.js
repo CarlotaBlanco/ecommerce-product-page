20ed553785e4b5a15fa715ad072967569c11f3f8
“CarlotaBlanco” <lotsilots@gmail.com>
2022-07-25 11:04:12 +0200

    add hamburger menu styles and collapse status

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('>> Ready :)');
+const hamburgerMenubutton = document.querySelector('.js-hamburger-menu');
+const hamburgerMenu = document.querySelector('.js-hamburger-menu-container');
+const closeMenu = document.querySelector('.js-close-menu');
+
+const opacity = document.querySelector('.js-opacity');
+
+function handleCollapseHamburgerMenu() {
+  hamburgerMenu.classList.remove('collapse');
+  opacity.classList.add('opacity');
+}
+
+function handleCloseHamburgerMenu() {
+  hamburgerMenu.classList.add('collapse');
+  opacity.classList.remove('opacity');
+}
+
+hamburgerMenubutton.addEventListener('click', handleCollapseHamburgerMenu);
+closeMenu.addEventListener('click', handleCloseHamburgerMenu);
